6c12ad08ae49e14eba6035c7eb29d4353dd894c3
Adeyemi Onibokun <ababaseun@gmail.com>
2024-11-17 22:48:35 +0100

    small fix

--- Product.Catalog.Services/Product.Catalog.Products/ProductService/Services/ProductService.cs
+++ Product.Catalog.Services/Product.Catalog.Products/ProductService/Services/ProductService.cs
@@ -7,13 +7,7 @@ namespace ProductService.Services;
 public class ProductService(AppDbContext ctx) : IProductService
 {
     public async Task<IEnumerable<Product>> GetProducts()
-    {
-        var x = Task.W
-        var products = ctx.Model.FindEntityType(typeof(Product)).GetSchema();
-        var product = ctx.Model.FindEntityType(typeof(Product)).GetProperties();
-
-        return await ctx.Products.ToListAsync();
-    }
+    => await ctx.Products.ToListAsync();
 
     public async Task<Product?> GetProductById(int id)
         => await ctx.Products.FirstOrDefaultAsync(p => p.ProductId == id);
