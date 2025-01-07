8d9ff9ced11dd59af5d1698d12cccb66b2d0f0a6
Adeyemi Onibokun <ababaseun@gmail.com>
2025-01-07 16:24:42 +0100

    Refactored webapi deployment  secrets

--- Product.Catalog.Services/Product.Catalog.Products/ProductService/Program.cs
+++ Product.Catalog.Services/Product.Catalog.Products/ProductService/Program.cs
@@ -9,12 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-
+var dbConnection = builder.Configuration.GetConnectionString("DbConnection");
+Console.WriteLine("*******************" + dbConnection);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection")));
+
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService.Services.ProductService>();
